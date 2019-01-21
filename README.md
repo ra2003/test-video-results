718cc3ec71dac0cbb1adb2fec279f7362f7412e8
jvmvik <jvmvik@gmail.com>
2019-01-21 11:56:33 -0600

    Add video results test for mobile and desktop (passing locally)

---
+++ README.md
@@ -0,0 +1,5 @@
+# Video Results desktop/mobile
+
+[![Build Status](https://travis-ci.org/serpapi/test-video-results.svg?branch=master)](https://travis-ci.org/serpapi/test-video-results)
+
+Tests for SerpApi desktop/mobile video results
---
+++ spec/video_results_coffee_spec.rb
@@ -0,0 +1,84 @@
+describe "SerpApi Desktop JSON" do
+
+  before(:all) do
+    @host = 'https://serpapi.com'
+    if ENV['SERPAPI_MODE'] == 'dev'
+       @host = 'http://localhost:3000'
+    end
+ end
+
+  describe "desktop: video results for Coffee" do
+
+    before :all do
+      @response = HTTP.get @host + '/search.json?q=Coffee&tbm=vid&location=Dallas&hl=en&gl=us&device=desktop&source=test'
+      @json = @response.parse
+    end
+
+    it "returns http success" do
+      expect(@response.code).to be(200)
+    end
+
+    it "contains video results array" do
+      expect(@json["video_results"]).to be_an(Array)
+    end
+
+    it 'contains 10 video in the result' do
+      expect(@json['video_results'].size).to eq(10)
+    end
+
+    describe "check video field are present for the first result" do
+
+      before :all do
+        @first_result = @json["video_results"][0]
+      end
+
+      %w(position title link displayed_link thumbnail snippet rich_snippet).each do |field|
+        it "#{field}" do
+           expect(@json["video_results"][0][field]).not_to be_nil
+        end
+      end
+
+      it "title" do
+        expect(@first_result["title"]).to_not be_empty
+      end
+    end
+  end
+
+  describe "mobile: video results for Coffee" do
+
+    before :all do
+      @response = HTTP.get @host + '/search.json?q=Coffee&tbm=vid&location=Dallas&hl=en&gl=us&device=mobile&source=test'
+      @json = @response.parse
+      File.write('cache.json', JSON.pretty_generate(@json))
+    end
+
+    it "returns http success" do
+      expect(@response.code).to be(200)
+    end
+
+    it "contains video results array" do
+      expect(@json["video_results"]).to be_an(Array)
+    end
+
+    it 'contains 10 video in the result' do
+      expect(@json['video_results'].size).to eq(10)
+    end
+
+    describe "check if field is present for the first result: " do
+
+      before :all do
+        @first_result = @json["video_results"][0]
+      end
+
+      %w(position title link thumbnail snippet).each do |field|
+        it "#{field}" do
+           expect(@json["video_results"][0][field]).not_to be_nil
+        end
+      end
+
+      it "title" do
+        expect(@first_result["title"]).to_not be_empty
+      end
+    end
+  end
+end
